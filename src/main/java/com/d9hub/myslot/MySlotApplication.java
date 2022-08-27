b0e943c123062e8d6a7a8dc3493fd0f83deaa614
Lahiru Ruhunage <lahiru.ruhunage@gtnexus.com>
2022-08-27 15:11:26 +0530

    Initial commit of my slot api

---
+++ src/main/java/com/d9hub/myslot/MySlotApplication.java
@@ -0,0 +1,13 @@
+package com.d9hub.myslot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySlotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySlotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/d9hub/myslot/MySlotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d9hub.myslot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySlotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
